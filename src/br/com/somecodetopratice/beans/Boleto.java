625eb75ad46411fed516eb32ea96601e2999bd87
Valdilanio <dot_v@hotmail.com>
2011-07-31 00:15:34 -0300

    Custom xml write with xstream

---
+++ src/br/com/somecodetopratice/beans/Boleto.java
@@ -0,0 +1,127 @@
+package br.com.somecodetopratice.beans;
+
+public class Boleto {
+	
+	public void setCEDENTE(String cEDENTE) {
+		CEDENTE = cEDENTE;
+	}
+	public String getCEDENTE() {
+		return CEDENTE;
+	}
+	
+	public String getBANCO() {
+		return BANCO;
+	}
+	public void setBANCO(String bANCO) {
+		BANCO = bANCO;
+	}
+	public String getNUMEROAGENCIA() {
+		return NUMEROAGENCIA;
+	}
+	public void setNUMEROAGENCIA(String nUMEROAGENCIA) {
+		NUMEROAGENCIA = nUMEROAGENCIA;
+	}
+	public String getNUMEROCONTA() {
+		return NUMEROCONTA;
+	}
+	public void setNUMEROCONTA(String nUMEROCONTA) {
+		NUMEROCONTA = nUMEROCONTA;
+	}
+	public String getASSINATURA() {
+		return ASSINATURA;
+	}
+	public void setASSINATURA(String aSSINATURA) {
+		ASSINATURA = aSSINATURA;
+	}
+	public String getDATAEMISSAO() {
+		return DATAEMISSAO;
+	}
+	public void setDATAEMISSAO(String dATAEMISSAO) {
+		DATAEMISSAO = dATAEMISSAO;
+	}
+	public String getDATAPROCESSAMENTO() {
+		return DATAPROCESSAMENTO;
+	}
+	public void setDATAPROCESSAMENTO(String dATAPROCESSAMENTO) {
+		DATAPROCESSAMENTO = dATAPROCESSAMENTO;
+	}
+	public String getDATAVENCIMENTO() {
+		return DATAVENCIMENTO;
+	}
+	public void setDATAVENCIMENTO(String dATAVENCIMENTO) {
+		DATAVENCIMENTO = dATAVENCIMENTO;
+	}
+	public String getNOMESACADO() {
+		return NOMESACADO;
+	}
+	public void setNOMESACADO(String nOMESACADO) {
+		NOMESACADO = nOMESACADO;
+	}
+	public String getENDERECOSACADO() {
+		return ENDERECOSACADO;
+	}
+	public void setENDERECOSACADO(String eNDERECOSACADO) {
+		ENDERECOSACADO = eNDERECOSACADO;
+	}
+	public String getCIDADESACADO() {
+		return CIDADESACADO;
+	}
+	public void setCIDADESACADO(String cIDADESACADO) {
+		CIDADESACADO = cIDADESACADO;
+	}
+	public String getUFSACADO() {
+		return UFSACADO;
+	}
+	public void setUFSACADO(String uFSACADO) {
+		UFSACADO = uFSACADO;
+	}
+	public String getCEPSACADO() {
+		return CEPSACADO;
+	}
+	public void setCEPSACADO(String cEPSACADO) {
+		CEPSACADO = cEPSACADO;
+	}
+	public String getCPFSACADO() {
+		return CPFSACADO;
+	}
+	public void setCPFSACADO(String cPFSACADO) {
+		CPFSACADO = cPFSACADO;
+	}
+	public String getNUMEROPEDIDO() {
+		return NUMEROPEDIDO;
+	}
+	public void setNUMEROPEDIDO(String nUMEROPEDIDO) {
+		NUMEROPEDIDO = nUMEROPEDIDO;
+	}
+	public String getVALORDOCUMENTOFORMATADO() {
+		return VALORDOCUMENTOFORMATADO;
+	}
+	public void setVALORDOCUMENTOFORMATADO(String vALORDOCUMENTOFORMATADO) {
+		VALORDOCUMENTOFORMATADO = vALORDOCUMENTOFORMATADO;
+	}
+	public String getSHOPPINGID() {
+		return SHOPPINGID;
+	}
+	public void setSHOPPINGID(String sHOPPINGID) {
+		SHOPPINGID = sHOPPINGID;
+	}
+	
+	private String CEDENTE;
+	private String BANCO;
+	private String NUMEROAGENCIA;
+	private String NUMEROCONTA;
+	private String ASSINATURA;
+	private String DATAEMISSAO;
+	private String DATAPROCESSAMENTO;
+	private String DATAVENCIMENTO;
+	private String NOMESACADO;
+	private String ENDERECOSACADO;
+	private String CIDADESACADO;
+	private String UFSACADO;
+	private String CEPSACADO;
+	private String CPFSACADO;
+	private String NUMEROPEDIDO;
+	private String VALORDOCUMENTOFORMATADO;
+	private String SHOPPINGID;
+
+}
---
+++ src/br/com/somecodetopratice/xstream/driver/TestCustomDriver.java
@@ -0,0 +1,28 @@
+package br.com.somecodetopratice.xstream.driver;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import br.com.somecodetopratice.BoletoWriterDriver;
+import br.com.somecodetopratice.beans.Boleto;
+
+import com.thoughtworks.xstream.XStream;
+
+
+public class TestCustomDriver{
+
+	public static void main(String[] args) throws FileNotFoundException {
+		   
+		   BoletoWriterDriver writer = new BoletoWriterDriver(new PrintWriter(new File("./resources/boleto.xml")));
+		   writer.setEndRoot("END_BOLETO_DESCRIPTION");
+		   XStream xs = new XStream();
+		   xs.alias("BEGIN_BOLETO_DESCRIPTION", Boleto.class);
+		   Boleto b = new Boleto();
+		   b.setASSINATURA("Vlad");
+		   b.setBANCO("237");
+		   b.setCEDENTE("Bradesco");
+		   xs.marshal(b, writer);
+	}
+
+}
